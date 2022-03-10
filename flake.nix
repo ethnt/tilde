9441c4aaed23d26c8bdcbd3d2c907a1f9ef36533
Ethan Turkeltaub <ethan@turkeltaub.dev>
2022-03-10 00:11:18 -0500

    Start with Digga

--- flake.nix
+++ flake.nix
@@ -6,8 +6,12 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixpkgs-master.url = "github:nixos/nixpkgs/master";
 
-    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.url = "github:montchr/nix-darwin/add-toplevel-option-lib";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    digga.url = "github:divnix/digga/darwin-support";
+    digga.inputs.darwin.follows = "nix-darwin";
+    digga.inputs.home-manager.follows = "home-manager";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
@@ -17,60 +21,18 @@
   };
 
   outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, nixpkgs-master, nix-darwin
-    , home-manager, utils, ... }:
-    utils.lib.mkFlake {
+    , digga, home-manager, utils, ... }:
+    digga.lib.mkFlake {
       inherit self inputs;
-
-      supportedSystems = [ "aarch64-darwin" "x86_64-darwin" ];
 
       channelsConfig.allowUnfree = true;
 
-      channels.nixpkgs.overlaysBuilder = channels:
-        [
-          (final: prev: {
-            unstable = nixpkgs-unstable.legacyPackages."${prev.system}";
-            master = nixpkgs-master.legacyPackages."${prev.system}";
-          })
-        ];
-
-      hostDefaults = {
-        output = "darwinConfigurations";
-        builder = nix-darwin.lib.darwinSystem;
-
-        system = "x86_64-darwin";
-        channelName = "nixpkgs";
-
-        modules = [
-          home-manager.darwinModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-          }
-        ];
+      channels = {
+        nixpkgs = { };
+        nixpkgs-unstable = { };
+        nixpkgs-master = { };
       };
 
-      hosts = { vm = { }; };
-
-      outputsBuilder = channels:
-        with channels.nixpkgs; {
-          devShell = mkShell { buildInputs = [ git nixfmt statix ]; };
-
-          checks = let
-            runCodeAnalysis = name: command:
-              runCommand "tilde-${name}-check" { } ''
-                cd ${self}
-                ${command}
-                mkdir $out
-              '';
-          in {
-            format = runCodeAnalysis "format" ''
-              ${nixfmt}/bin/nixfmt --check flake.nix
-            '';
-
-            lint = runCodeAnalysis "lint" ''
-              ${statix}/bin/statix check --format stderr
-            '';
-          };
-        };
+      nixos = { hostDefaults = { channelName = "nixpkgs"; }; };
     };
 }
