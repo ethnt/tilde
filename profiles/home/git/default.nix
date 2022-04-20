855b829199a7a5953265555e6337629f098c0048
Ethan Turkeltaub <ethan.turkeltaub@hey.com>
2022-04-20 12:46:04 -0400

    Update tmux styling

--- profiles/home/git/default.nix
+++ profiles/home/git/default.nix
@@ -1,4 +1,6 @@
-{
+{ pkgs, ... }: {
+  home.packages = with pkgs; [ hub ];
+
   programs.git = {
     enable = true;
 
--- profiles/home/tmux/default.nix
+++ profiles/home/tmux/default.nix
@@ -12,15 +12,23 @@
     baseIndex = 1;
     escapeTime = 0;
 
-    plugins = with pkgs; [ tmuxPlugins.nord ];
-
-    # TODO: Evaluate if powerline is too slow
     extraConfig = ''
       # Turn on mouse mode
       set -g mouse on
 
-      # Use Powerline
-      # run-shell "${pkgs.powerline}/bin/powerline-config tmux setup"
+      set -g status-style "bg=color233,fg=color247"
+
+      set -g status-left-length "120"
+      set -g status-left "#[bg=color226,fg=color0,bold] ❐ #S #[bg=color199,fg=color15,nobold] ↑ #(uptime | cut -f 4-5 -d ' ' | cut -f 1 -d ',') #[bg=color233] "
+
+      set-window-option -g window-status-style bg=colour233,fg=colour247
+      set-window-option -g window-status-current-style bg=colour39,fg=colour233,bold
+      set-window-option -g window-status-last-style bg=colour236,fg=colour39
+      set-window-option -g window-status-format " #I #W#{?window_zoomed_flag,*Z,} "
+      set-window-option -g window-status-current-format " #I #W#{?window_zoomed_flag,*Z,} "
+
+      set -g status-left-length "120"
+      set -g status-right '%H:%M | %Y-%m-%d #[bg=color160,fg=color15] #(whoami) #[bg=color15,fg=color233,bold] #h '
     '';
   };
 }
