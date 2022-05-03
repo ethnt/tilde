b80b9844f87c2055002d435c571149d3886e41b3
Ethan Turkeltaub <eturkeltaub@stripe.com>
2022-05-03 10:21:28 -0400

    Add a few tweaks for st-eturkeltaub1

--- hosts/st-eturkeltaub1/default.nix
+++ hosts/st-eturkeltaub1/default.nix
@@ -4,4 +4,7 @@
   tilde.system = { name = "st-eturkeltaub1"; };
 
   networking.hostName = config.tilde.system.name;
+
+  # Don't override the SSH known hosts file, it is managed by Stripe IT
+  environment.etc."ssh/ssh_known_hosts".enable = false;
 }
