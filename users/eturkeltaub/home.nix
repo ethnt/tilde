8719b6ba1a48d8581e602d2a2033f9c501c423ac
Ethan Turkeltaub <82977258+eturkeltaub-stripe@users.noreply.github.com>
2022-05-11 11:41:26 -0400

    Add `tmuxp` module (#30)

--- users/eturkeltaub/home.nix
+++ users/eturkeltaub/home.nix
@@ -42,4 +42,21 @@ with lib;
       ruby = { disabled = true; };
     };
   };
+
+  programs.tmuxp.workspaces = {
+    tilde = {
+      session_name = "tilde";
+      windows = [{
+        focus = true;
+        layout = "c5e0,212x37,0,0,3";
+        options = { };
+        panes = [{
+          focus = "true";
+          shell_command = "direnv reload";
+        }];
+        start_directory = "/Users/eturkeltaub/personal/tilde";
+        window_name = "fish";
+      }];
+    };
+  };
 }
