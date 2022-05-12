48f14d7289f0103bd72abfe400c3e0a414351040
Ethan Turkeltaub <ethan.turkeltaub@hey.com>
2022-05-11 23:54:59 -0400

    Update `tmuxp` workspaces (#33)

--- users/ethan/home.nix
+++ users/ethan/home.nix
@@ -18,4 +18,36 @@ with lib;
     userName = "Ethan Turkeltaub";
     signing.key = config.tilde.home.gpg.keyId;
   };
+
+  programs.tmuxp.workspaces = {
+    tilde = {
+      session_name = "tilde";
+      windows = [{
+        focus = true;
+        layout = "c5e0,212x37,0,0,3";
+        options = { };
+        panes = [{
+          focus = true;
+          shell_command = "direnv reload";
+        }];
+        start_directory = "/Users/ethan/Workspace/tilde";
+        window_name = "fish";
+      }];
+    };
+
+    orchard = {
+      session_name = "orchard";
+      windows = [{
+        focus = true;
+        layout = "c5e0,212x37,0,0,3";
+        options = { };
+        panes = [{
+          focus = true;
+          shell_command = "direnv reload";
+        }];
+        start_directory = "/Users/ethan/Workspace/orchard";
+        window_name = "fish";
+      }];
+    };
+  };
 }
