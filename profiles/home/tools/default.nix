f778124e731eef612929f85ba0d312577af0fda4
Ethan Turkeltaub <eturkeltaub@stripe.com>
2022-09-29 09:38:05 -0700

    Add some aliases

--- profiles/home/tools/default.nix
+++ profiles/home/tools/default.nix
@@ -20,4 +20,12 @@
     sd
     tldr
   ];
+
+  home.shellAliases = {
+    # Alias `rg` to `rg -p`
+    rg = "${pkgs.ripgrep}/bin/rg -p";
+
+    # Alias `man` to `batman`
+    man = "${pkgs.bat-extras.batman}/bin/batman";
+  };
 }
