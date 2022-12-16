40e8779a117f286a01d980b889ebdab956b04edf
Ethan Turkeltaub <eturkeltaub@stripe.com>
2022-12-16 10:08:11 -0500

    Add neovim configuration

---
+++ profiles/home/neovim/default.nix
@@ -0,0 +1,27 @@
+{ pkgs, ... }: {
+  programs.neovim = {
+    enable = true;
+
+    vimAlias = true;
+
+    extraConfig = ''
+      ${builtins.readFile ./base.vim}
+    '';
+
+    plugins = with pkgs.vimPlugins; [
+      ale
+      deoplete-nvim
+      fzf-vim
+      lightline-vim
+      nerdtree
+      solarized
+      tabular
+      vim-autoformat
+      vim-commentary
+      vim-elixir
+      vim-fugitive
+      vim-markdown
+      vim-nix
+    ];
+  };
+}
