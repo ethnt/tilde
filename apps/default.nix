b51cfda4791b0e918c8ae5d0be10ba7c6ceb0eaf
Ethan Turkeltaub <82977258+eturkeltaub-stripe@users.noreply.github.com>
2023-05-04 09:18:34 -0700

    Fix `remote` profile's `$PATH`, add app to unlock secrets (#87)
    * Fix pathing on remote profile
    
    * Add app for unlocking with git-crypt
    
    * Allow unlock app to take file or string
    
    * Use unlock app in GitHub Actions
    
    * Actually use unlock app in GitHub Actions
    
    * Maybe fix Actions?
    
    * Maybe fix Actions?
    
    * Maybe fix Actions?
    
    * Don't use unlock app on Actions yet
    
    * Don't use unlock app on Actions yet

--- apps/default.nix
+++ apps/default.nix
@@ -11,6 +11,27 @@
     '';
   };
 
+  unlock = self.inputs.flake-utils.lib.mkApp {
+    drv = pkgs.writeShellScriptBin "unlock" ''
+      GIT_CRYPT_KEY=/tmp/git-crypt-key
+
+      if [ -z "''${1}" ]; then
+        echo "Requires an argument of a file or base64-encoded string"
+        exit 1;
+      fi
+
+      if [ -f "''${1}" ]; then
+        cat "''${1}" | base64 -d > $GIT_CRYPT_KEY
+      else
+        echo "''${1}" | base64 -d > $GIT_CRYPT_KEY
+      fi
+
+      ${pkgs.git-crypt}/bin/git-crypt unlock $GIT_CRYPT_KEY
+
+      rm $GIT_CRYPT_KEY
+    '';
+  };
+
   repl = self.inputs.flake-utils.lib.mkApp {
     drv = pkgs.writeShellScriptBin "repl" ''
       if [ -z "$1" ]; then
