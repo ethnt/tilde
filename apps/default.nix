0f3aea6559ab464bdb58a8289b5e966adfc7e753
Ethan Turkeltaub <eturkeltaub@stripe.com>
2023-05-04 11:06:17 -0400

    Use unlock app in GitHub Actions

--- apps/default.nix
+++ apps/default.nix
@@ -13,13 +13,22 @@
 
   unlock = self.inputs.flake-utils.lib.mkApp {
     drv = pkgs.writeShellScriptBin "unlock" ''
-      if [ -f $1 ]
-      then cat $1 | base64 -d > /tmp/git-crypt-key
-      else echo $1 | base64 -d > /tmp/git-crypt-key
+      GIT_CRYPT_KEY=/tmp/git-crypt-key
+
+      if [ -z "''${1}" ]; then
+        echo "Requires an argument of a file or base64-encoded string"
+        exit 1;
       fi
 
-      ${pkgs.git-crypt}/bin/git-crypt unlock $(cat $1 | base64 -d)
-      rm /tmp/git-crypt-key
+      if [ -f "''${1}" ]; then
+        cat "''${1}" | base64 -d > $GIT_CRYPT_KEY
+      else
+        echo "''${1}" | base64 -d > $GIT_CRYPT_KEY
+      fi
+
+      # ${pkgs.git-crypt}/bin/git-crypt unlock $GIT_CRYPT_KEY
+
+      rm $GIT_CRYPT_KEY
     '';
   };
 
