97cc811a7e5a7290e524d0e5aa3464d346575f99
Ethan Turkeltaub <ethan.turkeltaub@hey.com>
2023-10-14 14:59:35 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "tilde";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
+
+      imports = [ ];
+
+      perSystem = _: { };
+
+      flake = { };
+    };
+}
