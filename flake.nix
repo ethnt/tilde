1c85a43241ecb3e053be24ca3a4db31d89501bba
Ethan Turkeltaub <eturkeltaub@stripe.com>
2023-12-04 09:52:46 -0500

    Add `aarch64-linux` as supported system

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-darwin" ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
 
       imports = [
         ./lib
