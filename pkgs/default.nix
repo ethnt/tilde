0c29f57a7d8965be657c8da4d74f8715ef4670d3
Ethan Turkeltaub <eturkeltaub@stripe.com>
2024-01-02 15:47:04 -0500

    Use latest Starship

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,4 +3,22 @@ _final: prev: {
   sf-pro = prev.callPackage ./fonts/sf-pro.nix { };
   nix-docker = prev.callPackage ./nix-docker.nix { };
   oh-my-tmux = prev.callPackage ./oh-my-tmux.nix { };
+  starship = prev.starship.overrideAttrs (oldAttrs: rec {
+    version = "1.17.1";
+
+    src = prev.fetchFromGitHub {
+      owner = "starship";
+      repo = "starship";
+      rev = "v${version}";
+      hash = "sha256-e+vhisUzSYKUUoYfSaQwpfMz2OzNcZbeHgbvyPon18g=";
+    };
+
+    cargoDeps = oldAttrs.cargoDeps.overrideAttrs (prev.lib.const {
+      name = "starship-vendor.tar.gz";
+      inherit src;
+      outputHash = "sha256-JAdYrdM11a1gquqL1ijjaEdMNYWHMWT5HI8+6eym+do=";
+    });
+
+    cargoHash = "sha256-xLlZyLvS9AcXQHxjyL4Dden1rEwCLB8/comfRyqXXCI=";
+  });
 }
