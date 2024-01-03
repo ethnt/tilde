80e584b90d81676871204bb17bfc4202a639a2bc
Ethan Turkeltaub <eturkeltaub@stripe.com>
2024-01-03 14:31:13 -0500

    Add nixvim

--- modules/profiles/home/neovim/default.nix
+++ modules/profiles/home/neovim/default.nix
@@ -1,29 +1,34 @@
 { config, pkgs, ... }: {
-  programs.neovim = {
+  programs.nixvim = {
     enable = true;
-
-    vimAlias = true;
-
-    extraConfig = ''
-      ${builtins.readFile ./base.vim}
-    '';
-
-    plugins = with pkgs.vimPlugins; [
-      ale
-      deoplete-nvim
-      fzf-vim
-      lightline-vim
-      nerdtree
-      solarized
-      tabular
-      vim-autoformat
-      vim-commentary
-      vim-elixir
-      vim-fugitive
-      vim-markdown
-      vim-nix
-    ];
+    plugins.lightline.enable = true;
   };
+
+  # programs.neovim = {
+  #   enable = true;
+
+  #   vimAlias = true;
+
+  #   extraConfig = ''
+  #     ${builtins.readFile ./base.vim}
+  #   '';
+
+  #   plugins = with pkgs.vimPlugins; [
+  #     ale
+  #     deoplete-nvim
+  #     fzf-vim
+  #     lightline-vim
+  #     nerdtree
+  #     solarized
+  #     tabular
+  #     vim-autoformat
+  #     vim-commentary
+  #     vim-elixir
+  #     vim-fugitive
+  #     vim-markdown
+  #     vim-nix
+  #   ];
+  # };
 
   home.sessionVariables.EDITOR = pkgs.lib.getExe config.programs.neovim.package;
 }
