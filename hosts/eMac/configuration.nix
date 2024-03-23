c14f9cc040d44bb78c40ad6e53204d693fd2033c
Ethan Turkeltaub <ethan.turkeltaub@hey.com>
2024-03-23 01:03:55 -0400

    Fixes to build the aarch64-linux devShell

--- hosts/eMac/configuration.nix
+++ hosts/eMac/configuration.nix
@@ -1,7 +1,10 @@
 { suites, profiles, ... }: {
   imports = with suites;
-    base ++ fonts ++ identity
-    ++ [ profiles.users.ethan profiles.builders.builder ];
+    base ++ fonts ++ identity ++ [
+      profiles.users.ethan
+      profiles.builders.builder
+      profiles.builders.darwin-linux
+    ];
 
   tilde.host = {
     name = "eMac";
