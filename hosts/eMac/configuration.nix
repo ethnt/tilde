2dabc4434d2d6c0e92f5c8e6846a719fbe713071
Ethan Turkeltaub <ethan.turkeltaub@hey.com>
2024-04-15 23:11:58 -0400

    Format code

--- hosts/eMac/configuration.nix
+++ hosts/eMac/configuration.nix
@@ -15,7 +15,18 @@
   homebrew = {
     brews = [ "pinentry-mac" ];
 
-    casks = [ "arc" "docker" "hey" "notion" "tailscale" "utm" "vlc" "zoom" "zed" "balenaetcher" ];
+    casks = [
+      "arc"
+      "docker"
+      "hey"
+      "notion"
+      "tailscale"
+      "utm"
+      "vlc"
+      "zoom"
+      "zed"
+      "balenaetcher"
+    ];
 
     masApps = { "Amphetamine" = 937984704; };
   };
