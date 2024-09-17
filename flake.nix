134dee6ff1e1829d33653feb4e1acae786a02fb4
Ethan Turkeltaub <ethan.turkeltaub@hey.com>
2024-09-17 16:40:45 -0400

    Bump `maxJobs` of `builder`

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     ];
 
     extra-trusted-public-keys = [
-      "tilde:S3A/pBaZ9qtdelB79faUEpgfuG0vrfqy9S/gcCjRnfo="
+      "tilde:Urwh7kyCsSsNmV1gu4sMYFVZCI0ZPwEXjxxtNH06G8U="
       "tilde.cachix.org-1:vjup2ixrsWKk+v8FXCqusKWBRwU0l7EzumjnMV4n2Vg="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
