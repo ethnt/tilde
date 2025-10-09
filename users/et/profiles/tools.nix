139406c74984419df695e89915d8fd9127712ae3
Ethan Turkeltaub <et@mercury.com>
2025-10-09 11:23:19 -0400

    Add `fnm`

--- users/et/profiles/tools.nix
+++ users/et/profiles/tools.nix
@@ -1,1 +1,8 @@
-{ pkgs, ... }: { home.packages = with pkgs; [ claude-code ]; }
+{ pkgs, ... }: {
+  home.packages = with pkgs; [ claude-code fnm mkcert ];
+
+  programs.fish.interactiveShellInit = let fnm = pkgs.lib.getExe pkgs.fnm;
+  in ''
+    ${fnm} env --use-on-cd --shell fish | source
+  '';
+}
