904c833628c20102130dc98625122c1ae7c7f266
Ethan Turkeltaub <ethan@turkeltaub.dev>
2026-04-11 17:02:31 -0400

    Update from tilde-secrets

--- flake.nix
+++ flake.nix
@@ -43,8 +43,7 @@
     treefmt.url = "github:numtide/treefmt-nix";
     treefmt.inputs.nixpkgs.follows = "nixpkgs";
 
-    # tilde-secrets.url = "git+ssh://git@github.com/ethnt/tilde-secrets";
-    tilde-secrets.url = "git+file:///Users/ethan/Workspace/tilde-secrets";
+    tilde-secrets.url = "git+ssh://git@github.com/ethnt/tilde-secrets";
 
     emacs-overlay.url = "github:nix-community/emacs-overlay";
     emacs-overlay.inputs.nixpkgs.follows = "nixpkgs";
