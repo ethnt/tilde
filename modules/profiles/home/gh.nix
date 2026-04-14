5cf63ec96f27e9771843f7839ba8a1a9796ff145
Ethan Turkeltaub <ethan@turkeltaub.dev>
2026-04-13 22:48:10 -0400

    Add `gh-stack` (#191)

--- modules/profiles/home/gh.nix
+++ modules/profiles/home/gh.nix
@@ -1,12 +1,15 @@
-{ config, lib, ... }: {
+{ config, lib, pkgs, ... }: {
   programs.gh = {
     enable = true;
+    extensions = [ pkgs.gh-stack ];
     settings = {
       aliases = {
         co = "pr checkout";
         pv = "pr view";
       };
-      editor = "${lib.getExe config.programs.vscode.package} --wait";
+      editor = "${lib.getExe config.programs.zed-editor.package} --wait";
     };
   };
+
+  programs.gh-dash.enable = true;
 }
