b59a00ba7edf1515aa4a929ca04b0a13c0bd84c1
Ethan Turkeltaub <et@mercury.com>
2026-04-30 09:47:44 -0400

    Use `zed-editor` from `nixpkgs-master`, update `nixpkgs-master`

--- modules/overlays/default.nix
+++ modules/overlays/default.nix
@@ -11,7 +11,7 @@
     in {
       nixpkgs-master-darwin-linux-builder = nixpkgs-master.darwin.linux-builder;
 
-      inherit (nixpkgs-master) ghostty-bin worktrunk;
+      inherit (nixpkgs-master) ghostty-bin worktrunk zed-editor;
     };
   };
 }
