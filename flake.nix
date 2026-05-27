9e3a830c814f137c05e2292a57a28009771cec08
Ethan Turkeltaub <ethan@turkeltaub.dev>
2026-05-27 10:44:15 -0400

    Use versioned branches for `nixpkgs`, `home-manager`, `nix-darwin` inputs (#198)
    * Use versioned branch for `nixpkgs` input
    
    * Use versioned branch for `home-manager` and `nix-darwin` inputs

--- flake.nix
+++ flake.nix
@@ -24,16 +24,18 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-26.05-darwin";
+
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     nixpkgs-master.url = "github:nixos/nixpkgs";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
 
-    nix-darwin.url = "github:nix-darwin/nix-darwin";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-26.05";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     flake-root.url = "github:srid/flake-root";
